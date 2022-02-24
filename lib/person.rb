cc277e2bf53f010732c4866ce703d54139afa3d0
Francois Spies <fcspies@gmail.com>
2022-02-24 16:48:09 -0500

    commit tree 61dd4206208f4b8944ea915c3afc8663b7be9f28

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
+require 'pry'
+
 class Person
 
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    binding.pry
+
 end
+
+
+# Define a name getter and a name= setter method for the instance variable @name.
+# Define a job getter and a job= setter method for the instance variable @job.
